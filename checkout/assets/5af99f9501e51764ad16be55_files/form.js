f5d57461554e0429ca54eb7ae7f9f92268513575
jjapexx <38406128+jjapexx@users.noreply.github.com>
2018-05-14 15:54:22 +0100

    Changes to card container

---
+++ checkout/assets/5af99f9501e51764ad16be55_files/form.js
@@ -0,0 +1,8 @@
+
+          jsclient.injectForm(document.querySelector("form"), {
+            "no_style": false,
+            "payment_products": ["card"],
+            "css_framework": "bootstrap",
+            "organisation_id": "37267ee50a1928cb9c6525cf"
+          })
+        
